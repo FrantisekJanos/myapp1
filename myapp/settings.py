c5a166f643f6b263cadf49bf2ecb9d3841d05bb7
frantisek <frantisek.janos91@gmail.com>
2023-09-14 23:30:54 +0200

    this is my third commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-shdvj9f6bc^+y6=*a%5r%=-99))di7sh87ya6lf82wmzs$powd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
